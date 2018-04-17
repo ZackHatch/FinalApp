b3c4d73f597956048f099b84c2d0a31ce445ba73
Zachary B Hatch <zh86549@srcs.loc>
2018-04-17 12:05:07 -0700

    Fix

--- app2.js
+++ app2.js
@@ -1,1 +1,12 @@
 Start
+var enterbutton = document.getElementById("textbox");
+enterbutton.addEventListener('click', () => {
+    document.getElementById('output').textContent = bingocheck()
+});
+
+function putingrid(){
+    var input = document.getElemenById("textbox").value;
+   var array = [];
+    array.push(input);
+
+}
