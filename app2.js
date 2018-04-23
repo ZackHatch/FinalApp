00ece2d779ba2021ed4155a43199a446feddac20
Zachary B Hatch <zh86549@srcs.loc>
2018-04-23 11:18:48 -0700

    change

--- app2.js
+++ app2.js
@@ -1,35 +1,34 @@
-function putinarray(){
+function putinarray() {
     var input = document.getElementById("item").value;
-   var someArray = str.split(",");
+    var someArray = str.split(",");
     someArray.push(input);
 }
 
-function shuffle (someArray) {
+function shuffle(someArray) {
     for (var i = someArray.length - 1; i > 0; i--) {
-         var j = Math.floor(Math.random() * (i + 1));
-         var x = someArray[i];
+        var j = Math.floor(Math.random() * (i + 1));
+        var x = someArray[i];
         someArray[i] = someArray[j];
         someArray[j] = x;
     }
     console.log(someArray);
     return someArray;
 }
-function Array(){
-    var array = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"," 17", "18"," 19", "20", "21", "22", "23", "24"]
+var array = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", " 17", "18", " 19", "20", "21", "22", "23", "24"]
 
-function insertText () {
+function insertText() {
     document.getElementById('').innerHTML = "Some text to enter";
 }
 
 //for loop that adds the input to each cell
 //cpounter that shows how many things left you can oninput
 //generate button that uses for loop to put t into the cells
-    //alert that pops up if you try to generate before it is full
-    //alert that pops up if you try to add more than 24
+//alert that pops up if you try to generate before it is full
+//alert that pops up if you try to add more than 24
 
 
 
-    var button = document.getElementById('addList');
+var button = document.getElementById('addList');
 button.addEventListener('click', additem)
 
 function additem() {
@@ -39,7 +38,7 @@ function additem() {
     deletebutton.textContent = "Delete";
     deletebutton.className = "delete"
     deletebutton.addEventListener('click', () => {
-         deletebutton.parentElement.remove();
+        deletebutton.parentElement.remove();
     });
     li.textContent = input + "";
     li.appendChild(deletebutton);
