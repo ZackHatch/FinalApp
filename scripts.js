3de558c9c3b23401bbbc7d0ab99814e3b86ce5ec
BuildTools <unconfigured@null.spigotmc.org>
2018-05-18 22:49:25 -0700

    more restructuring, sophisticated export button

--- scripts.js
+++ scripts.js
@@ -1,14 +1,26 @@
 var array = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
     cardtemplate = document.getElementById("bingo"),
-    resetButton = document.getElementById('reset'),
-    addbutton = document.getElementById('addList'),
-    generateButton = document.getElementById('generate'),
+    resetButton = document.getElementById("reset"),
+    addbutton = document.getElementById("addList"),
+    generateButton = document.getElementById("generate"),
     hideButton = document.getElementById("hide"),
+    importbutton = document.getElementById("import"),
+    exportbutton = document.getElementById("export"),
+    inputbox = document.getElementById("item"),
+    position = document.getElementById("position"),
+    displayparent = document.getElementById("displayparent"),
     arrayreadout = document.getElementById("displayArray");
+
 hideButton.addEventListener("click", hidelist);
-resetButton.addEventListener('click', reset);
-addbutton.addEventListener('click', additem);
-generateButton.addEventListener('click', generateArray);
+addbutton.addEventListener("click", additem);
+generateButton.addEventListener("click", generateArray);
+importbutton.addEventListener("click", importarray);
+position.addEventListener("click", inputboxliveupdate);
+exportbutton.addEventListener("click", exportarray);
+resetButton.addEventListener("click", () => {
+    window.location.reload()
+});
+
 setInterval(updatelist, 5)
 
 function shuffle(someArray) {
@@ -23,11 +35,14 @@ function shuffle(someArray) {
 }
 
 function additem() {
-    var input = document.getElementById("item").value,
-        position = document.getElementById("position");
-    array[position.value] = input;
+    array[position.value] = inputbox.value;
     console.log(array);
-    ++position.value
+    if (position.value == 23) {
+        position.value = 0
+    } else {
+        ++position.value
+    }
+    inputboxliveupdate()
 }
 
 function generateArray() {
@@ -39,11 +54,11 @@ function generateArray() {
 }
 
 function hidelist() {
-    if (arrayreadout.hidden == false) {
-        arrayreadout.hidden = true
+    if (displayparent.hidden == false) {
+        displayparent.hidden = true
         hideButton.textContent = "Unhide List"
     } else {
-        arrayreadout.hidden = false
+        displayparent.hidden = false
         hideButton.textContent = "Hide List"
     }
 }
@@ -51,13 +66,42 @@ function hidelist() {
 function updatelist() {
     arrayreadout.textContent = ""
     for (i = 0; i < array.length; i++) {
-        arrayreadout.textContent = arrayreadout.textContent + array[i] + " (" + i + "), "
         if (i == array.length - 1) {
             arrayreadout.textContent = arrayreadout.textContent + array[i] + " (" + i + ")"
+        } else {
+            arrayreadout.textContent = arrayreadout.textContent + array[i] + " (" + i + "), "
         }
     }
 }
 
-function reset() {
-    window.location.reload()
+function importarray() {
+    var promptentry = prompt("Copy and paste the export list from another instance"),
+        importedarray = promptentry.split(",");
+    if (importedarray.length == 24) {
+        array = importedarray
+        console.log("Import Successful")
+        inputboxliveupdate()
+    } else {
+        alert("Import Array Invalid")
+    }
+}
+
+function exportarray() {
+    for (i = 0; i < array.length; i++) {
+        if (array[i] == "") {
+            complete = false
+            break
+        } else {
+            complete = true
+        }
+    }
+    if (complete == true) {
+        alert(array)
+    } else {
+        alert("Cannot export incomplete list")
+    }
+}
+
+function inputboxliveupdate() {
+    inputbox.value = array[position.value]
 }
